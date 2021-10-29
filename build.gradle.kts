e63e7ac8bcc80f1e0256849a6136f2df51566346
Svilen Ivanov <svilen.ivanov@gmail.com>
2021-10-29 18:16:07 +0300

    Make it runnable

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "1.5.31"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.graphql-java-kickstart:graphql-java-tools:12.0.0")
+}
+
+application.mainClass.set("Main")
+
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+import graphql.kickstart.tools.GraphQLQueryResolver
+import graphql.kickstart.tools.SchemaParser
+
+
+interface TestInterface {
+    val testUnion: TestUnion
+}
+
+interface TestUnion
+
+class TestUnion1(val a: String?) : TestUnion
+class TestUnion2(val b: String?) : TestUnion
+
+class TestInterfaceImpl(
+    override val testUnion: TestUnion1,
+    val c: String,
+) : TestInterface
+
+class Schema : GraphQLQueryResolver {
+    fun query(): TestInterface = TestInterfaceImpl(TestUnion1("a"), "b")
+    fun dummy(): TestUnion = TestUnion1("a")
+}
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        SchemaParser
+            .newParser()
+            .files("schema.graphql")
+            .resolvers(Schema())
+            .dictionary(
+                TestInterfaceImpl::class.java,
+                TestUnion1::class.java,
+                TestUnion2::class.java,
+                TestUnion::class.java,
+                TestInterface::class.java,
+            )
+            .build()
+            .makeExecutableSchema()
+    }
+}
